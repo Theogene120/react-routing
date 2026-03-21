d5c55bdce448cc9e4749a201419e40237289be01
Theogene <igiranezatheogene4@gmail.com>
2026-03-21 02:01:12 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
-function App() {
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+function Home(){
   return (
-    <h1 className='text-5xl font-bold text-purple-600'>Hello world</h1>
+    <h1 className='text-5xl font-bold text-purple-600 text-center'>Hello React Router</h1>
   )
 }
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+export function About(){
+  return(
+    <h1 className="text-center text-2xl font-extrabold">This is about page</h1>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
