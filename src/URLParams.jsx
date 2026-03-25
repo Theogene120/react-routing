d84a1767673ef6968ffec7c8c8593444d7b6dd1e
Theogene <igiranezatheogene4@gmail.com>
2026-03-25 16:44:05 +0200

    Add url params

--- src/URLParams.jsx
+++ src/URLParams.jsx
@@ -1,9 +1,27 @@
-import { BrowserRouter, Route, Routes, useParams, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useParams, NavLink } from "react-router-dom";
 
 function Info(){
     const {firstname} = useParams()
 
     return(
-        <h1>Hello, {firstname}</h1>
+        <h1 className="ml-5 text-2xl font-extrabold">Hello, {firstname}</h1>
     )
 }
+
+function URLParams(){
+
+    return(
+        <BrowserRouter>
+            <nav className='m-5 flex gap-10 font-semibold text-lg'>
+                <NavLink className={({isActive}) => isActive ? "text-purple-500" : "hover:underline"} to='/John'>John</NavLink>
+                <NavLink className={({isActive}) => isActive ? "text-purple-500" : "hover:underline"} to='/Peter'>Peter</NavLink>
+                <NavLink className={({isActive}) => isActive ? "text-purple-500" : "hover:underline"} to='/Joseph'>Joseph</NavLink>
+            </nav>
+            <Routes>
+                <Route path="/:firstname" element={<Info />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default URLParams;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { About } from './App'
+import URLParams from './URLParams'
 
 
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <URLParams />
 )
