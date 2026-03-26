40327b9d8270cd414f23b44fb66fbc2087e1f257
Theogene <igiranezatheogene4@gmail.com>
2026-03-26 23:06:09 +0200

    use of useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -11,25 +11,26 @@ createRoot(document.getElementById('root')).render(
 )
 
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useMemo } from "react";
 
 function Test() {
-  const [inputValue, setInputValue] = useState("");
-  const count = useRef(0);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
+  const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    return num * 2;
+  };
+
+  const result = useMemo(() => {
+    return expensiveCalculation(count);
+  }, [count]);
 
   return (
     <>
-      <p>Type in the input field: </p>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <h1>Render Count: {count.current}</h1>
+      <h1>Result: {result}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <input className='ml-2 border border-black outline-none' value={text} onChange={(e) => setText(e.target.value)} />
     </>
   );
 }
