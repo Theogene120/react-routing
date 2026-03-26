e0cb8dcb71e964ec2c1ee3b0da82084865083abd
Theogene <igiranezatheogene4@gmail.com>
2026-03-27 00:14:02 +0200

    custom hook

--- src/CustomHook.jsx
+++ src/CustomHook.jsx
@@ -4,12 +4,12 @@ function CustomHook(){
     const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
 
     return (
-        <>
+        <ul className="ml-10 flex flex-col gap-4 list-disc">
             {
                 data && 
-                data.map(item => <p id={item.id}>{item.title}</p>)
+                data.map(item => <li className="" id={item.id}>{item.title}</li>)
             }
-        </>
+        </ul>
     )
 }
 
