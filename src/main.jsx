7c4a443b2df6d28bdd1ebdd97101635cea2e0373
Theogene <igiranezatheogene4@gmail.com>
2026-03-26 23:00:49 +0200

    done with useRef

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import App from './App'
 import { About } from './App'
 import URLParams from './URLParams'
@@ -6,5 +7,29 @@ import URLParams from './URLParams'
 
 
 createRoot(document.getElementById('root')).render(
-  <URLParams />
+  <Test />
 )
+
+
+import { useState, useRef, useEffect } from 'react';
+
+function Test() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <>
+      <p>Type in the input field: </p>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
+}
