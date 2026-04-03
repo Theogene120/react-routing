68cc93d874b1753b59a62e60f5cd6d0b283fb3a5
Theogene <igiranezatheogene4@gmail.com>
2026-04-03 15:44:42 +0200

    Add Not Fund page displayed by default

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,16 @@ function Bikes(){
   )
 }
 
+function NotFound() {
+  return(
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
+
+
 export function Product(){
   return(
     <div>
