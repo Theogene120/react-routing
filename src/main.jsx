9d6aa24a5f7c22bb09921c0f9b1bc83ca235079a
Theogene <igiranezatheogene4@gmail.com>
2026-04-04 16:51:14 +0200

    preload teh component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App'
-import { About } from './App'
 import URLParams from './URLParams'
 import CustomHook from './CustomHook'
 import { useNavigate } from 'react-router-dom'
@@ -9,39 +8,39 @@ import { useNavigate } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  <URLParams />
+  <App />
 )
 
 
 
-function Test() {
-  const [count, setCount] = React.useState(0);
-  const [text, setText] = React.useState("");
+// function Test() {
+//   const [count, setCount] = React.useState(0);
+//   const [text, setText] = React.useState("");
 
-  const expensiveCalculation = (num) => {
-    console.log("Calculating...");
-    return num * 2;
-  };
+//   const expensiveCalculation = (num) => {
+//     console.log("Calculating...");
+//     return num * 2;
+//   };
 
-  const result = React.useMemo(() => {
-    return expensiveCalculation(count);
-  }, [count]);
+//   const result = React.useMemo(() => {
+//     return expensiveCalculation(count);
+//   }, [count]);
 
-  return (
-    <>
-      <h1>Result: {result}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <input className='ml-2 border border-black outline-none' value={text} onChange={(e) => setText(e.target.value)} />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <h1>Result: {result}</h1>
+//       <button onClick={() => setCount(count + 1)}>Increase</button>
+//       <input className='ml-2 border border-black outline-none' value={text} onChange={(e) => setText(e.target.value)} />
+//     </>
+//   );
+// }
 
-function LogIn() {
-  const navigate = useNavigate()
+// function LogIn() {
+//   const navigate = useNavigate()
   
-  function handleSubmit(e) {
-    e.preventDefault()
-    // different logic
-    navigate('/dashboard', {relative: true})
-  }
-}
+//   function handleSubmit(e) {
+//     e.preventDefault()
+//     // different logic
+//     navigate('/dashboard', {relative: true})
+//   }
+// }
