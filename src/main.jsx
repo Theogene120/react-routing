9ded63b82a24b3782c368efb5a83b707082b3080
Theogene <igiranezatheogene4@gmail.com>
2026-04-18 22:18:31 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
+
 
 import './index.css'
 import React, { StrictMode } from 'react'
@@ -14,31 +13,35 @@ import Counter from './Hooks/UseReducerHook'
 import Search from './Hooks/UseCallbackHook'
 import CalculateWithMemo from './Hooks/useMemoHook'
 import Custom from './Hooks/CustomHook2'
+import Theme from './Theme'
 
 
+
+// Redux staff
+
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import AppRedux from './AppRedux'
 import userReducer from './feactures/user'
 
 
-import Theme from './Theme'
-
 const store = configureStore({
-  reducer: {
+  reducer: {  // function that hold information of current state and the action that will be implemented on that state
     user: userReducer,
   }
 })
 
-// createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <CalculateWithMemo />
-//   </Provider>
-// )
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Theme />
-  </StrictMode>
+  <Provider store={store}>
+    <AppRedux />
+  </Provider>
 )
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Theme />
+//   </StrictMode>
+// )
 
 
 
