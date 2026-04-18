c34cfb9929c277792281cd20a1785c1fd3d19a64
Theogene <igiranezatheogene4@gmail.com>
2026-04-18 22:20:10 +0200

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,17 @@
+import { useDispatch } from "react-redux";
+import { login, logout } from "../feactures/user";
 
+function Login() {
+  const dispatch = useDispatch();
 
-function Login(){
-
-    return (
-        <div className="flex justify-center mt-10"><button className="border border-black p-4 bg-gray-300">LogIn</button></div>
-    )
+  return (
+    <div className="flex justify-center mt-10">
+      <button className="border border-black p-4 bg-gray-300"
+      onClick={() => {dispatch(login({name: 'Theo', age: 20, email: 'theo@gmail.com'}))}}
+      >LogIn</button>
+      <button className="border border-black p-4 bg-gray-300" onClick={() => {dispatch(logout())}}>Logout</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
