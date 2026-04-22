af041cfb7a325a1982b984df06df7240faf7b83c
Theogene <igiranezatheogene4@gmail.com>
2026-04-22 14:57:44 +0200

    change colors component

--- src/pages/ChangeColor.jsx
+++ src/pages/ChangeColor.jsx
@@ -7,9 +7,9 @@ function ChangeColor() {
     const [color, setColor] = useState('')
     const dispatch = useDispatch()
     return (
-        <div>
-            <input type="text" onChange={(e) => setColor(e.target.value)}/>
-            <button onClick={() => {dispatch(changeColor(color))}}>CHANGE COLOR</button>
+        <div className="m-10 text-center gap-4 flex justify-center"> 
+            <input className="outline-none border-2 p-1 border-black" type="text" onChange={(e) => setColor(e.target.value)}/>
+            <button className="border-2 bg-blue-400 text-white border-black p-2 font-bold text-sm rounded-md" onClick={() => {dispatch(changeColor(color))}}>CHANGE COLOR</button>
         </div>
     )
 }
