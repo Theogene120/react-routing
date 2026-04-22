e49ab2c0d9a26b717f7a403be953a6e21a7238c1
Theogene <igiranezatheogene4@gmail.com>
2026-04-22 14:58:18 +0200

    changes on profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,9 +5,10 @@ import { useSelector } from "react-redux"
 function Profile(){
 
     const user = useSelector((state) => state.user.value)
+    const themeColor = useSelector((state) => state.theme.value)
 
     return (
-        <div className="text-center mt-10">
+        <div className={`text-center mt-10`} style={{color: themeColor}}>
             <div className="text-blue-500 text-2xl font-bold">Profile Page</div>
             <p className="text-lg">Name : {user.name}</p>
             <p className="text-lg">Age : {user.age}</p>
