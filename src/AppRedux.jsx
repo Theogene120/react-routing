5625f3f5303e6bd8e84824a85f490a535db6c42f
Theogene <igiranezatheogene4@gmail.com>
2026-04-28 23:05:57 +0200

    changes

--- src/AppRedux.jsx
+++ src/AppRedux.jsx
@@ -1,5 +1,6 @@
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
+import ChangeColor from "./pages/ChangeColor"
 
 
 function AppRedux() {
@@ -7,6 +8,7 @@ function AppRedux() {
         <div>
             <Profile />
             <Login />
+            <ChangeColor />
         </div>
     )
 }
